1e51943d3f17bda4fd51d9b8f8d755ddd500e8ab
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-07 14:09:43 -0300

    Initial commit

---
+++ src/main/java/com/guilherme/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,14 @@
+package com.guilherme.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
