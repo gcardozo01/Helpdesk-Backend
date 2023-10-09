0d049f045e87c0cbfb1037afe6b528bfbbc38cb0
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-09 14:44:01 -0300

    JPA annotations for domain classes

--- src/main/java/com/guilherme/helpdesk/domain/Chamado.java
+++ src/main/java/com/guilherme/helpdesk/domain/Chamado.java
@@ -1,21 +1,44 @@
 package com.guilherme.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.guilherme.helpdesk.domain.enums.Prioridade;
 import com.guilherme.helpdesk.domain.enums.Status;
 
-public class Chamado {
+@Entity
+public abstract class Chamado implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento = LocalDate.now();
+	
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
 	public Chamado() {
--- src/main/java/com/guilherme/helpdesk/domain/Tecnico.java
+++ src/main/java/com/guilherme/helpdesk/domain/Tecnico.java
@@ -3,8 +3,15 @@ package com.guilherme.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa {
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
