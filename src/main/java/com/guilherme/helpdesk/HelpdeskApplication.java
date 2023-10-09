46a78508972df1c5efaec1ad82f036bd7167b569
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-09 15:41:30 -0300

    initial database load

--- src/main/java/com/guilherme/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/guilherme/helpdesk/HelpdeskApplication.java
@@ -1,14 +1,49 @@
 package com.guilherme.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import com.guilherme.helpdesk.domain.Chamado;
+import com.guilherme.helpdesk.domain.Cliente;
+import com.guilherme.helpdesk.domain.Tecnico;
+import com.guilherme.helpdesk.domain.enums.Perfil;
+import com.guilherme.helpdesk.domain.enums.Prioridade;
+import com.guilherme.helpdesk.domain.enums.Status;
+import com.guilherme.helpdesk.repositories.ChamadoRepository;
+import com.guilherme.helpdesk.repositories.ClienteRepository;
+import com.guilherme.helpdesk.repositories.TecnicoRepository;
+
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "63653230268", "guilherme@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@mail.com", "123");
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
