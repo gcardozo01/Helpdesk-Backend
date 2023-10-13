da15a50370643740b6114b8a3236c51370948c63
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-13 19:40:28 -0300

    Exception for technician not found

--- src/main/java/com/guilherme/helpdesk/services/TecnicoService.java
+++ src/main/java/com/guilherme/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.guilherme.helpdesk.domain.Tecnico;
 import com.guilherme.helpdesk.repositories.TecnicoRepository;
+import com.guilherme.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
-		return tecnico.orElse(null);
+		return tecnico.orElseThrow(() -> new ObjectNotFoundException("Técnico não encontrado! Id: " + id));
 	}
 }
---
+++ src/main/java/com/guilherme/helpdesk/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.guilherme.helpdesk.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+}
