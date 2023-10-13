08e3627a8d9bb586037937a4c7b566545c60fb40
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-13 19:17:39 -0300

    DTO standard for technician

--- src/main/java/com/guilherme/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/guilherme/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.helpdesk.domain.Tecnico;
+import com.guilherme.helpdesk.domain.dtos.TecnicoDTO;
 import com.guilherme.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResource {
 	private TecnicoService tecnicoService;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico tecnico = this.tecnicoService.findById(id);
-		return ResponseEntity.ok().body(tecnico);
+		return ResponseEntity.ok().body(new TecnicoDTO(tecnico));
 	}
 }
