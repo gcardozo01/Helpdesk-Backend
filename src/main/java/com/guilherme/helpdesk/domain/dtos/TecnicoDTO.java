e0445df5e1634fb9f155d306130810f2a0f3222a
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-15 14:58:28 -0300

    validation in the new user creation fields

--- src/main/java/com/guilherme/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/guilherme/helpdesk/domain/dtos/TecnicoDTO.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.CollectionTable;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.guilherme.helpdesk.domain.Tecnico;
@@ -17,10 +18,19 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	protected Integer id;
+	
+	@NotNull(message = "O campo NOME é requerido")
 	protected String nome;
+	
+	@NotNull(message = "O campo CPF é requerido")
 	protected String cpf;
+	
+	@NotNull(message = "O campo E-MAIL é requerido")
 	protected String email;
+	
+	@NotNull(message = "O campo SENHA é requerido")
 	protected String senha;
+	
 	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
 	
--- src/main/java/com/guilherme/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/guilherme/helpdesk/resources/TecnicoResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,7 +41,7 @@ public class TecnicoResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO tecnicoDTO) {
+	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO tecnicoDTO) {
 		Tecnico tecnico = tecnicoService.create(tecnicoDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(tecnico.getId()).toUri();
 		return ResponseEntity.created(uri).build();
